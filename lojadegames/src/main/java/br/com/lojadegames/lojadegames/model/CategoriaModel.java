1f8ecdce975020ce68db71908b113525be5f4663
SuelenBento <suelenbeento@gamil.com>
2022-02-23 21:45:21 -0300

    CRUD LojaDeGames

---
+++ lojadegames/src/main/java/br/com/lojadegames/lojadegames/model/CategoriaModel.java
@@ -0,0 +1,81 @@
+package br.com.lojadegames.lojadegames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 20)
+	private String tipo;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String preco;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String genero;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
+
+	
+	
+	
+
+}
